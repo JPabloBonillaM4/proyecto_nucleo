87a1caf1ed5b3890a7f739ab70a2f4f1b97bbeaf
J Pablo <jpm_caprico@hotmail.com>
2020-02-07 00:42:03 -0600

    nuevas funciones JS

--- formulario.php
+++ formulario.php
@@ -14,27 +14,33 @@
                     <h3 class="m-0 font-weight-bold">¡Bienvenido a nucleo capital!</h3>
                     <p class="m-0 py-3">Favor de llenar los datos de registro</p>
                     <form action="">
-                        <div>
+                        <!-- SECCION-1 -->
+                        <div id="seccion1">
                             <div class="form-group">
-                                <input type="text" class="form-control w-100 py-4 border border-dark" placeholder="Nombre(s)">
+                                <input type="text" class="form-control w-100 py-4 border border-dark" placeholder="Nombre(s)" name="name" id="name">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control py-4 border border-dark" placeholder="Primer apellido">
-                                <input type="text" class="form-control py-4 border border-dark" placeholder="Segundo apellido">
-                                <input type="mail" class="form-control py-4 border border-dark" placeholder="Email">
-                                <input type="phone" class="form-control py-4 border border-dark" placeholder="Celular / WhatssApp">
-                                <input type="password" class="form-control py-4 border border-dark" placeholder="Crear contraseña">
-                                <input type="password" class="form-control py-4 border border-dark" placeholder="Confirmar contraseña">
-                                <input type="text" class="form-control py-4 border border-dark" placeholder="RFC con el que facturas" maxlength="13" oninput="mayusculas(this);">
+                                <input type="text" class="form-control py-4 border border-dark" placeholder="Primer apellido" name="first_surname">
+                                <input type="text" class="form-control py-4 border border-dark" placeholder="Segundo apellido" name="second_surname">
+                                <input type="mail" class="form-control py-4 border border-dark" placeholder="Email" name="email" id="email">
+                                <input type="tel" class="form-control py-4 border border-dark" placeholder="Celular / WhatssApp" name="phone" id="phone" maxlength="10">
+                                <input type="password" class="form-control py-4 border border-dark" placeholder="Crear contraseña" name="password1">
+                                <input type="password" class="form-control py-4 border border-dark" placeholder="Confirmar contraseña" name="password2">
+                                <input type="text" class="form-control py-4 border border-dark" placeholder="RFC con el que facturas" maxlength="13" oninput="mayusculas(this);" name="rfc">
                                 <div class="d-flex align-items-center">
                                     <label for="" class="w-50 text-center">Porcentaje de acciones: </label>
-                                    <input type="text" class="form-control py-4 border border-dark ml-auto mr-0" style="width:45%;">
+                                    <input type="text" class="form-control py-4 border border-dark ml-auto mr-0 text-center" style="width:45%;" name="actions" maxlength="2" id="actions">
                                 </div>
                             </div>
                             <div class="d-flex justify-content-between align-items-center mt-5 registro_botones">
                                 <a href="#" onclick="window.history.back();" class="text-center"><i class="fas fa-chevron-left"></i> Regresar</a>
-                                <a href="#" class="text-white py-1 px-5 rounded font-weight-bold text-center">Continuar</a>
+                                <button class="text-white py-1 px-5 rounded font-weight-bold text-center" id="continuar_seccion2">Continuar</button>
                             </div>
+                        </div>
+                        <!-- SECCION-2 -->
+                        <div id="seccion2" style="display:none;">
+                            <h2>SECCION 2</h2>
+                            <a href="#">Regresar</a>
                         </div>
                     </form>
                 </div>
@@ -44,6 +50,22 @@
     </div>
 
 
+
+    <!-- ALERTAS -->
+    <div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
+        <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            <p></p>
+        </div>
+    </div>
+    
     <?php include('includes/footer.php'); ?>
 
 <?php include('includes/principal/foot.php'); ?>
--- js/main.js
+++ js/main.js
@@ -2,3 +2,80 @@
 function mayusculas(event){
     event.value = event.value.toUpperCase();
 }
+
+$(document).ready(function(){
+    // VARIABLES Y ACCIONES DE INICIO
+    let data = {};
+    $('#name').focus();actions
+    $('#phone').on('keypress',validarNumerico);
+    $('#actions').on('keypress',validarNumerico);
+
+    // VALIDACION DE SECCION 1
+    $('#continuar_seccion2').click(function(event){
+        event.preventDefault();
+        let validarCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('#email').val()),
+            validacion_pass;
+
+        $('#seccion1 input').each(function(){
+            data[this.name] = this.value;
+        });
+
+        validacion_pass = validacionContraseñas(data.password1,data.password2);
+
+        if(data.name != '' && data.first_surname != '' && data.second_surname && data.email != '' && data.phone != '' && data.password1 != '' && data.password2 != '' && data.rfc != '' && data.actions != ''){
+
+            if(!validarCorreo)
+                alert('Escriba una dirección de correo válida');
+
+            if(validacion_pass.error)
+                alert(validacion_pass.mensaje);
+
+            if(validarCorreo && !validacion_pass.error)
+                mostrarSeccion('#seccion1');
+    
+        } else {
+            alert('Favor de llenar todos los campos');
+            return false;
+        }
+    });
+
+    // VALIDAR CONTRASEÑAS
+    function validacionContraseñas(pass1, pass2){
+        let respuesta = {};
+
+        if((pass1.length >= 8 && pass1.length <= 16) && (pass1.length >= 8 && pass1.length <= 16))
+        {
+            if(pass1 === pass2)
+            {
+                respuesta['error']   = false;
+            } else {
+                respuesta['mensaje'] = 'Las contraseñas escritas deben coincidir';
+                respuesta['error']   = true;
+            }
+    
+        } else {
+            respuesta['mensaje'] = 'La contraseña escrita debe ser mayor o igual a 8 y menor o igual a 16 caracteres';
+            respuesta['error']   = true;
+        }
+
+        return respuesta;
+
+    }
+
+    // VALIDAR NUMERO TELEFONICO
+    function validarNumerico(event){
+        if(event.which < 48 || event.which > 57)
+        {
+           event.preventDefault();
+        } 
+    }
+
+    // MOSTRAR SECCION SIGUIENTE DEL REGISTRO
+    function mostrarSeccion(seccion){
+        $(seccion).slideToggle('slow');
+        setTimeout(() => {
+            $(seccion).next().slideToggle('slow');
+        }, 1000);
+    }
+
+});
